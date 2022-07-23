12c7fc7274a4b1d9811a87a681d9423cf3504245
Sidelnikov Roman <79612731370@yandex.ru>
2022-07-23 22:56:53 +0300

    add secret token

--- DrugBot/Configs.cs
+++ DrugBot/Configs.cs
@@ -1,14 +1,25 @@
+#nullable enable
 using System;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace DrugBot
 {
+    [Serializable]
     public class Configs
     {
+        private const ushort SecretKey = 0x42;
+        private string? _token;
         public uint Id { get; set; }
 
-        public string Token { get; set; }
+        public string? SecretToken { get; set; }
+
+        public string Token
+        {
+            get => _token ??= EncodeDecrypt(SecretToken, SecretKey);
+            set => _token = value;
+        }
 
         private Configs()
         {
@@ -21,6 +32,16 @@ namespace DrugBot
             var configs = JsonConvert.DeserializeObject<Configs>(json);
 
             return configs;
+        }
+
+        private static string EncodeDecrypt(string? str, ushort secretKey)
+        {
+            return str?.ToArray().Aggregate("", (current, c) => current + TopSecret(c, secretKey)) ?? "";
+        }
+
+        private static char TopSecret(char character, ushort secretKey)
+        {
+            return (char)(character ^ secretKey);
         }
     }
 }
