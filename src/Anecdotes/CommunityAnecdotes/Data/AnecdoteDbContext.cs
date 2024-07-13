b0d7037ae42ef6932aeb04ae7a27637dfceb096a
kykaroo <boris_nemcev@mail.ru>
2024-07-13 13:55:18 +0500

    Добавлен класс для взаимодействия с базой данных

---
+++ src/Anecdotes/CommunityAnecdotes/Data/AnecdoteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Anecdotes.CommunityAnecdotes.Data;
+
+public class AnecdoteDbContext : DbContext
+{
+    public DbSet<CommulityAnecdoteData> AnecdoteDatas;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        BuildAnecdotes(modelBuilder);
+    }
+
+    private void BuildAnecdotes(ModelBuilder modelBuilder)
+    {
+        var configuration = modelBuilder.Entity<CommulityAnecdoteData>();
+    }
+}
---
+++ src/Anecdotes/CommunityAnecdotes/Data/CommulityAnecdoteData.cs
@@ -0,0 +1,8 @@
+﻿namespace Anecdotes.CommunityAnecdotes.Data;
+
+public class CommulityAnecdoteData
+{
+    public int Id { get; set; }
+    public ulong User { get; set; }
+    public string Anecdote { get; set; } = string.Empty;
+}
