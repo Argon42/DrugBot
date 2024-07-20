80e4005949c5ea986fbc79ceb0aafe958ca30a61
kykaroo <boris_nemcev@mail.ru>
2024-07-20 19:58:24 +0500

    Добавлена миграция для мудрости

---
+++ src/DrugBot.DataBase/Migrations/Chinese/20240720145756_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DrugBot.DataBase.Migrations.Chinese
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChineseSymbols",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChineseSymbol = table.Column<char>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChineseSymbols", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChineseSymbols");
+        }
+    }
+}
